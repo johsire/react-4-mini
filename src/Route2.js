5f8c632be948807824f1d7e9d7064da854b0129f
Josiah Maina <josiah5115@gmail.com>
2018-07-16 01:39:24 -0600

    set-up all routes

---
+++ src/Route2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Router2() {
+  return (
+    <div>
+      It's from the Router2 Comp
+    </div>
+  )
+}
---
+++ src/Route3.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Router3() {
+  return (
+    <div>
+      This is the Router3 Component!
+    </div>
+  )
+}
